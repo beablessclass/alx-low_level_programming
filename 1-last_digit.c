b1b2bf7521f8e5c289f941b9f3e8d9aea117eace
beablessclass <beatricealabison@gmail.com>
2023-08-13 21:42:33 -0700

    Update output messages in 1-last_digit.c

--- 1-last_digit.c
+++ 1-last_digit.c
@@ -2,10 +2,6 @@
 #include <time.h>
 #include <stdio.h>
 
-/**
- * main - Entry point
- * Return: Always return 0 (success)
-*/
 int main(void)
 {
     int n;
@@ -15,11 +11,11 @@ int main(void)
     n = rand() - RAND_MAX / 2;
     m = n % 10;
     if (m > 5)
-        printf("Last digit of %d is %d and is greater than 5\n", n, m);
+        printf("The last digit of %d is %d and is greater than 5\n", n, m);
     else if (m == 0)
-        printf("Last digit of %d is %d and is 0\n", n, m);
+        printf("The last digit of %d is %d and is 0\n", n, m);
     else
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+        printf("The last digit of %d is %d and is less than 6 and not 0\n", n, m);
 
     return (0);
 }
