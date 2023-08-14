bc93306f03936204c8fdab344946c97fa7ff98c6
beablessclass <beatricealabison@gmail.com>
2023-08-13 23:02:11 -0700

    0-positive_or_negative.c

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
                 
 /**
- * main - Entry point.
+ * main - Entry point
  * Return: Always 0 (Success)        
  */
 
@@ -14,10 +14,10 @@ int main(void)
     srand(time(0));
     n = rand() - RAND_MAX / 2;
     if (n > 0)
-        printf("%d is positive\n", n);
+            printf("%d is positive\n", n);
     else if (n == 0)
-        printf("%d is zero\n", n);
+            printf("%d is zero\n", n);
     else
-        printf("%d is negative\n", n);
+            printf("%d is negative\n", n);
     return (0);
 }
