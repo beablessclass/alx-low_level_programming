f2247b07e80a03628405795021f591d9f048b2b0
beablessclass <beatricealabison@gmail.com>
2023-08-14 22:34:50 -0700

    1-last_digit.c

--- 1-last_digit.c
+++ 1-last_digit.c
@@ -2,19 +2,27 @@
 #include <time.h>
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int n;
-    int m;
+	int n;
+	int last_digit;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    m = n % 10;
-    if (m > 5)
-        printf("The last digit of %d is %d and is greater than 5\n", n, m);
-    else if (m == 0)
-        printf("The last digit of %d is %d and is 0\n", n, m);
-    else
-        printf("The last digit of %d is %d and is less than 6 and not 0\n", n, m);
-    return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_digit = n % 10;
+
+	if (last_digit > 5)
+		printf("Last digit of %d is %d and is greater than 5\n",
+		       n, last_digit);
+	else if (last_digit == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+		       n, last_digit);
+
+	return (0);
 }
