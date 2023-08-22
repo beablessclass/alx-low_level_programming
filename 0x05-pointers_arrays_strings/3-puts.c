3e03805ae7d361c0db90a2ef326871d9520f8962
beablessclass <beatricealabison@gmail.com>
2023-08-21 18:32:43 -0700

    ALL Task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prfsdfsdfsdf b
+ * @str: pofsdfsfsdfsdf
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
