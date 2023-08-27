569e2c1c25d8a33833ada9a27864f948d369b806
beablessclass <beatricealabison@gmail.com>
2023-08-27 01:30:26 -0700

    update

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - starting point of the script
+ * main - starting point of script
  * Description: Obtain a random number and display it
  * classification as positive, negative, or zero
  * Return: 0
